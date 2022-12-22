43efb5ebeac7bd4b4f8ef1a8c6c6f6e0de412907
Swack <43678199+swackgit@users.noreply.github.com>
2022-12-21 21:49:25 -0600

    Create init.php

---
+++ init.php
@@ -0,0 +1,21 @@
+<?php class twitter_fix extends Plugin {
+        private $host;
+        function about() {
+                return array(1.0,
+                        "gets rid of line breaks between images so they wrap on widescreen",
+                        "swack");
+        }
+        function init($host) {
+                $this->host = $host;
+                $host->add_hook($host::HOOK_ARTICLE_FILTER, $this);
+        }
+        function hook_article_filter($article) {
+        	$result = str_replace("'><br>\n<br>","'>",$article["content"]);
+
+			$article["content"] = $result;
+                return $article;
+        }
+        function api_version() {
+                return 2;
+        }
+}
