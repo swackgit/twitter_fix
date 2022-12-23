d0ec40e09cc7785e9b08a9a0a3eeb2bb467ee2ff
Swack <43678199+swackgit@users.noreply.github.com>
2022-12-22 22:00:06 -0600

    Update init.php
    switch from twitter to https://pbs.twimg.com

--- init.php
+++ init.php
@@ -15,7 +15,7 @@
 			$article["content"] = $result;
 		
 		        $subject = $article["content"];
-        		$pattern = '~(<a.href="https:\/\/twitter.com\/profile_images)(.*?)(">.*?<\/a>)~mi';
+        		$pattern = '~(<a.href="https:\/\/pbs.twimg.com\/profile_images)(.*?)(">.*?<\/a>)~mi';
         		$replacement = '';
         		$article["content"] = preg_replace($pattern,$replacement,$subject);
 
